5ce4da0f5a34673f19d5c032608044caf00b5833
hopeforkoami <hopeforkoami@gmail.com>
2023-02-03 21:01:32 +0000

    Add my_enumerable module

---
+++ .github/workflows/my_list.rb
@@ -0,0 +1,19 @@
+require_relative './my_enumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(*args)
+    @list = args
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 }) # true
+puts(list.all? { |e| e > 5 }) # false
+puts(list.any? { |e| e == 2 }) # true
+puts(list.any? { |e| e == 5 }) # false
+puts(list.filter(&:even)) # [2, 4]
