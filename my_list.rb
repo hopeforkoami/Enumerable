cd95854e76aad2e24d5c2b3bf40781b54de6d37d
hopeforkoami <hopeforkoami@gmail.com>
2023-02-03 21:34:23 +0000

    correct rubocop errors

--- my_list.rb
+++ my_list.rb
@@ -16,4 +16,4 @@ puts(list.all? { |e| e < 5 }) # true
 puts(list.all? { |e| e > 5 }) # false
 puts(list.any? { |e| e == 2 }) # true
 puts(list.any? { |e| e == 5 }) # false
-puts(list.filter(&:even)) # [2, 4]
+puts(list.filter(&:even?)) # [2, 4]
